8a31a29a7d1bbecf6a6b7254a8925aa2b86a583b
Lucas2infjb <104460648+Lucas2infjb@users.noreply.github.com>
2022-04-26 16:17:29 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var express = require('express');
+
+var app = express();
+
+//Cria as rotas
+app.get('/', (req, res) => {
+      res.send("pagina pricipal")
+})
+//liagar o servidor
+app.listen(3000, () => {
+      console.log("servidor ligado")
+}) 
